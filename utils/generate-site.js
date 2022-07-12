8379683ce63be1ecf2c8e50a54dc7adc3c0c80b5
olliedirker <olliedirker@gmail.com>
2022-07-11 23:13:21 -0500

    uploaded with test index

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -1,22 +1,41 @@
 const fs = require('fs');
-const { resolve } = require('path');
-// const copyFile = () => {};
 
+// writing files
 const writeFile = fileContent => {
-    return new Promise((resolve, reject) => {
-        fs.writeFile('./dist/index.html', fileContent, err => {
-            //if there an error, reject the Promise and send the error to the promise's `.catch` method
-            if (err) {
-                //return out of the function here to make sure the Promise doesnt accidentally execute the resolve() function as well
-                return;
-            }
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./dist/index.html', fileContent, err => {
+      if (err) {
+        reject(err);
+        return;
+      }
 
-            //if everything went well, resolve the promise and send the successful data to the `.then()` method
-            resolve({
-                ok: true,
-                message: 'File created!'
-            });
-        });
+      resolve({
+        ok: true,
+        message: 'File created!'
+      });
     });
+  });
 };
-module.exports = { writeFile, copyFile } = require('./utils/generate-site.js');
+
+// copying file
+const copyFile = () => {
+  return new Promise((resolve, reject) => {
+    fs.copyFile('./src/style.css', './dist/style.css', err => {
+      if (err) {
+        reject({
+          ok: false,
+          message: 'Something went wrong!',
+          err
+        });
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message: 'Stylesheet created!'
+      });
+    });
+  });
+};
+
+module.exports = { writeFile, copyFile };
